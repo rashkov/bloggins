c46907c3e318c0520804b2ed4c5003d42bd4910d
Michael Rashkovsky <mrashkovsky@gmail.com>
2016-01-25 14:24:20 -0500

    Fix jsonapi ignoring underscored attributes
    jsonapi does not accept created_at and updated_at on the Ember side.
    This commit serializes them as updated and created to work around this.

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,12 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :body
+  attributes :id, :body, :created, :updated
+
+  # Need this because JSONAPISerializer doesn't work with underscore case
+  #   Such as in updated_at and created_at
+  def created
+    object.created_at
+  end
+  def updated
+    object.updated_at
+  end
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,13 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body
+  attributes :id, :title, :body, :created, :updated
   has_many :comments
+
+  # Need this because JSONAPISerializer doesn't work with underscore case
+  #   Such as in updated_at and created_at
+  def created
+    object.created_at
+  end
+  def updated
+    object.updated_at
+  end
 end
