d30b40b1393a00793d1e704da425566d6f852b4a
Michael Rashkovsky <mrashkovsky@gmail.com>
2016-01-25 14:18:56 -0500

    Support post creation and accept new mime type
    Was not receiving any data in my update method.
    Found a solution here:
    https://github.com/rails-api/active_model_serializers/issues/1027
    Need to support the jsonapi mime type application/vnd.api+json
    for params parser to work properly.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  include ActionController::ImplicitRender
+  respond_to :json
   def index
     @posts = Post.all
     render json: @posts, include:['comments']
@@ -7,5 +9,22 @@ class PostsController < ApplicationController
   def show
     @post = Post.find params[:id]
     render json: @post, include:['comments']
+  end
+
+  def create
+    attributes = {
+      title: create_params[:attributes][:title],
+      body: create_params[:attributes][:body]
+    }
+    @post = Post.create attributes
+    respond_with @post
+  end
+
+  def create_params
+    params.require(:data).permit(attributes: [:body, :title])
+  end
+
+  def update_params
+    params.require().permit()
   end
 end
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,2 +1,5 @@
 Mime::Type.unregister :json
 Mime::Type.register "application/json", :json, %w( text/x-json application/jsonrequest application/vnd.api+json )
+ActionDispatch::ParamsParser::DEFAULT_PARSERS[Mime::Type.lookup('application/vnd.api+json')]=lambda do |body|
+  JSON.parse(body)
+end
