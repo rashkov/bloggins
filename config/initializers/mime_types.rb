39ed1da3e438c7b174b6a1f65ca63fb0959bfde0
Michael Rashkovsky <mrashkovsky@gmail.com>
2017-03-26 22:31:38 -0400

    Check in files from "rails rake:update"

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,5 +1,4 @@
-Mime::Type.unregister :json
-Mime::Type.register "application/json", :json, %w( text/x-json application/jsonrequest application/vnd.api+json )
-ActionDispatch::ParamsParser::DEFAULT_PARSERS[Mime::Type.lookup('application/vnd.api+json')]=lambda do |body|
-  JSON.parse(body)
-end
+# Be sure to restart your server when you modify this file.
+
+# Add new mime types for use in respond_to blocks:
+# Mime::Type.register "text/richtext", :rtf
